89616f4f57e2af443e9c5b864412274498182385
PutriDwijanti <158579690+PutriDwijanti@users.noreply.github.com>
2025-02-04 21:00:48 +0700

    Add files via upload

---
+++ piketrpl/kelompok.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/styles_kelompok.css">
+</head>
+<body>
+<?php include "layout/header.html"; ?>
+
+<div class="top-bar">
+    <a href="petugas.php" class="petugas-text">Petugas Piket</a>
+    <span class="kelompok-label">Kelompok</span>
+</div>
+<?php include "layout/footer.html"; ?>
+</body>
+</html>
